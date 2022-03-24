c7901dd7530685c717f8422a39698c4858bb1dad
ebellotpu6 <ebellotpu6@gmail.com>
2022-03-24 13:51:01 +0100

    Developing the main sections and footer using classes and render function.

--- main.js
+++ main.js
@@ -44,6 +44,83 @@ class Category {
     }
 }
 
+// Product Model
+class Product {
+    constructor(id, name, type, image, price, init_price) {
+        this._id = id;
+        this._name = name;
+        this._type = type;
+        this._image = image;
+        this._price = price;
+        if (init_price === 0) this._init_price = price;
+        else this._init_price = init_price;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        this._name = name;
+    }
+
+    get type() {
+        return this._type;
+    }
+
+    set type(type) {
+        this._type = type;
+    }
+
+    get init_price() {
+        return parseFloat(this._init_price).toFixed(2);
+    }
+
+    set init_price(init_price) {
+        this._init_price = init_price;
+    }
+
+    get price() {
+        return parseFloat(this._price).toFixed(2);
+    }
+
+    set price(price) {
+        this._price = price;
+    }
+
+    get image() {
+        return this._image;
+    }
+
+    set image(image) {
+        this._image = image;
+    }
+
+    is_onSale() {
+        return this._init_price > this._price;
+    }
+}
+
+
+// Product Model
+class Variant extends Product{
+    constructor(id, name, type, ref, image, price, init_price = 0) {
+        super(id, name, type, image, price, init_price);
+        this._ref = ref;
+    }
+    get ref() {
+        return this._ref;
+    }
+
+    set ref(ref) {
+        this._ref = ref;
+    }
+}
+
 let categoryList = [
     new Category(1, "Infantiles", "./src/images/categories/infantiles.jpeg", [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]),
     new Category(2, "Patchwork", "./src/images/categories/patchwork.jpeg", [22,23,3,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41]),
@@ -183,8 +260,56 @@ let subCategoryList = [
     new Category(118, "Ofertas de hogar", "./src/images/categories/ofertas.jpeg")
 ];
 
+let productList = [
+    new Variant(1, "Cretona estampada", "Referencia", "Saule Vert", "./src/images/productos/Cretonne_Saule_Vert_m.jpeg", 8.95, 10.5),
+    new Variant(2, "Cretona estampada", "Referencia", "Taya Celadon", "./src/images/productos/Taya_Celadon_m.jpeg", 8.95, 10.5),
+    new Variant(3, "Cretona estampada", "Referencia", "Lokoli Vert", "./src/images/productos/Lokoli_Vert_m.jpeg", 8.95, 10.5),
+    new Variant(4, "Cretona estampada", "Referencia", "Fontaine Bleau Vert", "./src/images/productos/fontaine_bleau_vert_m.jpeg", 8.95, 10.5),
+    new Variant(5, "Cretona estampada", "Referencia", "Sanaga Vert", "./src/images/productos/Sanaga_Vert_m.jpeg", 8.95, 10.5),
+    new Variant(6, "Cretona estampada", "Referencia", "Janaina Safran", "./src/images/productos/janaina_safran_m.jpeg", 8.95, 10.5),
+    new Variant(7, "Cretona estampada", "Referencia", "Janaina Emeraude", "./src/images/productos/janaina_emeraude_m.jpeg", 8.95, 10.5),
+    new Variant(8, "Cretona estampada", "Referencia", "Menara Turquoise", "./src/images/productos/ALGODON_MENARA_TURQUOISE_m.jpeg", 8.95, 10.5),
+    new Variant(9, "Cretona estampada", "Referencia", "Lokoli Multicolore", "./src/images/productos/Lokoli_Multicolore_m.jpeg", 8.95, 10.5),
+    new Variant(10, "Cretona estampada", "Referencia", "Taya Marine", "./src/images/productos/Taya_marine_m.jpeg", 8.95, 10.5),
+    new Variant(11, "Cretona estampada", "Referencia", "Zika Multicolore", "./src/images/productos/Zika_Multicolore_m.jpeg", 8.95, 10.5),
+
+    new Variant(12, "LeQuilt", "Referencia", "Legumes 1", "./src/images/productos/Legumes_1_112.jpg", 9.95),
+    new Variant(13, "Telas de Cocina", "Referencia", "Sweer Treats", "./src/images/productos/Sweet_Treats.jpg", 11.5),
+    new Variant(14, "Cretona estampada", "Referencia", "Baguette Francaise Ivore", "./src/images/productos/Baguette_Francaise_Ivore.jpg", 8.95, 10.5),
+    new Variant(15, "LeQuilt", "Referencia", "Chataigne 4", "./src/images/productos/le_quilt_chataigne_4.jpg", 9.95),
+    new Variant(16, "LeQuilt", "Referencia", "Griotte 1", "./src/images/productos/griotte_1.jpg", 9.95),
+    new Variant(17, "Telas de Cocina", "Referencia", "Watermelon Delight", "./src/images/productos/watermelon.jpg", 11.5),
+    new Variant(18, "Telas de Cocina", "Referencia", "Happy Foods", "./src/images/productos/Happy_Foods.jpg", 11.5),
+    new Variant(19, "Telas de Cocina", "Referencia", "Tomato Slice", "./src/images/productos/slice_tomatoe.jpg", 11.5),
+
+    new Variant(20, "Pictures", "Referencia", "213 - Pokemon Ash y Pickachu", "./src/images/productos/Pictures_213.jpeg", 10.25),
+    new Variant(21, "Pictures", "Referencia", "228 - Mario Bros", "./src/images/productos/Pictures_228.jpeg", 10.25),
+    new Variant(22, "Pictures", "Referencia", "232 - Minecraft", "./src/images/productos/Pictures_232.jpeg", 10.25),
+    new Variant(23, "Pictures", "Referencia", "236 - DragonBall", "./src/images/productos/Pictures_236.jpeg", 10.25),
+    new Variant(24, "Marvel", "Referencia", "Los Defensores", "./src/images/productos/defenders_earth_m.jpeg", 10.25),
+    new Variant(25, "Pictures", "Referencia", "251 - Harry Potter", "./src/images/productos/broom.600_m.jpg", 10.25),
+
+    new Variant(26, "Waffle RS0228", "Color", "050 - Blanco", "./src/images/productos/waffle_blanco.jpeg", 9.8),
+    new Variant(27, "Waffle RS0228", "Color", "033 - Salmon", "./src/images/productos/waffle_salmon.jpeg", 9.8),
+    new Variant(28, "Waffle RS0228", "Color", "011 - Rosa", "./src/images/productos/waffle_rosa.jpeg", 9.8),
+    new Variant(29, "Waffle RS0228", "Color", "047 - Lila", "./src/images/productos/waffle_lila.jpeg", 9.8),
+    new Variant(30, "Waffle RS0228", "Color", "004 - Azul Polvoriento", "./src/images/productos/waffle_dusty-blue.jpeg", 9.8),
+    new Variant(31, "Waffle RS0228", "Color", "002 - Azul", "./src/images/productos/waffle_blue.jpeg", 9.8),
+    new Variant(32, "Waffle RS0228", "Color", "021 - Turquesa", "./src/images/productos/waffle_turquoise.jpeg", 9.8),
+    new Variant(33, "Waffle RS0228", "Color", "005 - Adriatic", "./src/images/productos/waffle_adriatic.jpeg", 9.8),
+    new Variant(34, "Waffle RS0228", "Color", "068 - Gris Oscuro", "./src/images/productos/waffle_dark-grey.jpeg", 9.8)
+];
+
+console.log(productList[0]);
+
+let productsNewArrivalsList = [1,2,3,4,5,6,7,8,9,10,11];
+let productTelasCocinaList = [12,13,14,15,16,17,18,19];
+let productsFavoritesList = [20,21,22,23,24,25];
+let productsWaffleList = [26,27,28,29,30,31,32,33,34];
+
 // View
 
+/* HEADER */ 
 const navbar_category_menu = document.getElementById("navbar_category_list");
 const navbar_categoryOverlayConatainer = document.getElementById("navbar_categoryOverlayConatainer");
 const navbar_subcategory_menu = document.getElementById("navbar_subcategory_list");
@@ -270,72 +395,84 @@ close_navbar_button.addEventListener("click", function(){
     sideBarMenu.style = "transform: translate3d(-100%, 0, 0);";
 });
 
-const render = () => {
-    navbar_category_menu.textContent = "";
-    navbar_sidePanel_category_menu.textContent = "";
-    for (let i = 0; i < categoryList.length; i++) {
-        navbar_category_menu.appendChild(renderNavbarMenuCategory(categoryList[i]));
-        navbar_sidePanel_category_menu.appendChild(renderNavbarSidePanelMenuCategory(categoryList[i]));
+/* -------------------------------------------------------------- */
+
+/* MAIN */ 
+const product_carousels = document.querySelectorAll(".product-list");
+const product_carousel_newarrivals = document.getElementById("carousel_newarrivals");
+const product_carousel_telascocina = document.getElementById("carousel_telascocina");
+const product_carousel_favorites = document.getElementById("carousel_favorites");
+const product_carousel_waffle = document.getElementById("carousel_waffle");
+
+const renderProductcarousel = (product) => {
+    const li = document.createElement("li");
+
+    const div_container = document.createElement("div");
+
+    const a_img = document.createElement("a");
+    const div_img = document.createElement("div");
+    const img_product = document.createElement("img");
+    img_product.classList.add("product-listItem-img");
+    img_product.src = product.image;
+    img_product.alt = product.name + "-" + product.ref;
+    img_product.sizes = "(max-width: 767px) calc(50vw - 36px), (min-width: 768px) and (max-width: 1280px) calc(25vw - 64px), 284px";
+    img_product.height = "433";
+    img_product.width = "300";
+    div_img.append(img_product);
+    a_img.append(div_img);
+
+    const div_favorite = document.createElement("div");
+    const button = document.createElement("button");
+    const img_favorite = document.createElement("img");
+    img_favorite.src = "./src/icons/heart.svg";
+    img_favorite.alt = "Añadir a favoritos";
+    button.append(img_favorite);
+    button.classList.add("z-button");
+    div_favorite.append(button);
+    div_favorite.classList.add("add-favoriteButton");
+
+    const div_productInfo = document.createElement("div");
+    const a = document.createElement("a");
+
+    const div_1 = document.createElement("div");
+    const div_name = document.createElement("div");
+    div_name.textContent = product.name;
+    div_name.classList.add("product-infoName");
+    const div_ref = document.createElement("div");
+    div_ref.textContent = product.type + ": " + product.ref; 
+    div_ref.classList.add("product-infoReference");
+    div_1.append(div_name, div_ref);
+
+    const div_2 = document.createElement("div");
+    const span_price = document.createElement("span");
+    span_price.textContent = product.price + "€/m";
+    span_price.classList.add("product-infoPrice");
+    div_2.append(span_price);
+    if (product.is_onSale()){
+        const span_initprice = document.createElement("span");
+        span_initprice.textContent = product.init_price + "€/m";
+        span_initprice.classList.add("product-infoPrice-onSale");
+        div_2.append(span_initprice);
     }
+
+    a.append(div_1, div_2);
+    div_productInfo.append(a);
+    div_productInfo.classList.add("product-info");
+
+    div_container.append(a_img, div_favorite, div_productInfo);
+    li.append(div_container);
+    li.classList.add("product-listItem");
+    return li;
 }
-
-render();
-
-
-
-
-
-/*
-
-const navbar_button = document.getElementById("navbar_button");
-const sideBarMenu = document.getElementById("navbar_sidePanel_container");
-const close_navbar_button = document.getElementById("close_navbar_button");
-const navbar_category_container = document.getElementById("navbar_category_container");
-const navbar_subMenu = document.querySelectorAll('.navbar-subMenu');
-const navbar_categoryOverlayConatainer = document.getElementById("navbar_categoryOverlayConatainer");
-const navbar_subMenuImage = document.getElementById("navbar_subMenuImage");
-const footer_accordion = document.querySelectorAll(".footer-section");
-
-const product_carousels = document.querySelectorAll('.product-list');
-
-
-
-//Handler submenu (show on mouseover)
-function showSubmenu(category) {
-    navbar_categoryOverlayConatainer.classList.add("navbar_categoryOverlayConatainer-open");
-    for(const element of navbar_subMenu){
-        if (element.attributes.reference.value == category){
-            element.style = "display: grid;"
-            navbar_subMenuImage.style=`background-image: url(./src/images/categories/${category}.jpeg);`
-        }
-        else element.style = "display: none";
-    }
-}
-
-//Handler submenu (hide on mouseleave)
-navbar_category_container.addEventListener("mouseleave", function(){
-    navbar_categoryOverlayConatainer.classList.remove("navbar_categoryOverlayConatainer-open");
-});
-
 
 //Handler product carousel
-for (let i = 0; i < product_carousels.length; i++){
-    let itemWidth = product_carousels[i].querySelector('.product-listItem').clientWidth;
-    product_carousels[i].nextElementSibling.querySelector("button.product-carousel-buttonLeft").addEventListener('click', function(){
-        product_carousels[i].scrollBy({left: -itemWidth, top: 0, behavior: 'smooth'});
-    });
-    product_carousels[i].nextElementSibling.querySelector("button.product-carousel-buttonRight").addEventListener('click', function(){
-        product_carousels[i].scrollBy({left: itemWidth, top: 0, behavior: 'smooth'});
-    });
-    product_carousels[i].onscroll = function () {
-        let prevProductCarousel = product_carousels[i].nextElementSibling.querySelector("button.product-carousel-buttonLeft");
-        let nextProductCarousel = product_carousels[i].nextElementSibling.querySelector("button.product-carousel-buttonRight");
-        if(product_carousels[i].scrollLeft == 0) prevProductCarousel.classList.add('hide-content');
-        else if(prevProductCarousel.classList.contains('hide-content')) prevProductCarousel.classList.remove('hide-content');
-        if(product_carousels[i].scrollLeft == (product_carousels[i].scrollWidth - product_carousels[i].offsetWidth)) nextProductCarousel.classList.add('hide-content');
-        else if(nextProductCarousel.classList.contains('hide-content')) nextProductCarousel.classList.remove('hide-content');
-    }
-}
+
+
+/* -------------------------------------------------------------- */
+
+/* FOOTER */ 
+
+const footer_accordion = document.querySelectorAll(".footer-section");
 
 //Handler footer sections
 for(let i = 0; i < footer_accordion.length; i++){
@@ -344,4 +481,56 @@ for(let i = 0; i < footer_accordion.length; i++){
     });
 }
 
-*/
+/* -------------------------------------------------------------- */
+
+/* RENDER */ 
+
+const render = () => {
+    navbar_category_menu.textContent = "";
+    navbar_sidePanel_category_menu.textContent = "";
+    for (let i = 0; i < categoryList.length; i++) {
+        navbar_category_menu.appendChild(renderNavbarMenuCategory(categoryList[i]));
+        navbar_sidePanel_category_menu.appendChild(renderNavbarSidePanelMenuCategory(categoryList[i]));
+    }
+
+    product_carousel_newarrivals.textContent = "";
+    for (let i = 0; i < productsNewArrivalsList.length; i++) {
+        product_carousel_newarrivals.appendChild(renderProductcarousel(productList[productsNewArrivalsList[i]-1]));
+    }
+    product_carousel_telascocina.textContent = "";
+    for (let i = 0; i < productTelasCocinaList.length; i++) {
+        product_carousel_telascocina.appendChild(renderProductcarousel(productList[productTelasCocinaList[i]-1]));
+    }
+    
+    product_carousel_favorites.textContent = "";
+    for (let i = 0; i < productsFavoritesList.length; i++) {
+        product_carousel_favorites.appendChild(renderProductcarousel(productList[productsFavoritesList[i]-1]));
+    }
+    
+    product_carousel_waffle.textContent = "";
+    for (let i = 0; i < productsWaffleList.length; i++) {
+        product_carousel_waffle.appendChild(renderProductcarousel(productList[productsWaffleList[i]-1]));
+    }
+
+    for (let i = 0; i < product_carousels.length; i++){
+        let itemWidth = product_carousels[i].querySelector('.product-listItem').clientWidth;
+        product_carousels[i].nextElementSibling.querySelector("button.product-carousel-buttonLeft").addEventListener('click', function(){
+            product_carousels[i].scrollBy({left: -itemWidth, top: 0, behavior: 'smooth'});
+        });
+        product_carousels[i].nextElementSibling.querySelector("button.product-carousel-buttonRight").addEventListener('click', function(){
+            product_carousels[i].scrollBy({left: itemWidth, top: 0, behavior: 'smooth'});
+        });
+        product_carousels[i].onscroll = function () {
+            let prevProductCarousel = product_carousels[i].nextElementSibling.querySelector("button.product-carousel-buttonLeft");
+            let nextProductCarousel = product_carousels[i].nextElementSibling.querySelector("button.product-carousel-buttonRight");
+            if(product_carousels[i].scrollLeft == 0) prevProductCarousel.classList.add('hide-content');
+            else if(prevProductCarousel.classList.contains('hide-content')) prevProductCarousel.classList.remove('hide-content');
+            if(product_carousels[i].scrollLeft == (product_carousels[i].scrollWidth - product_carousels[i].offsetWidth)) nextProductCarousel.classList.add('hide-content');
+            else if(nextProductCarousel.classList.contains('hide-content')) nextProductCarousel.classList.remove('hide-content');
+        }
+    }
+}
+
+render();
+
+/* -------------------------------------------------------------- */
